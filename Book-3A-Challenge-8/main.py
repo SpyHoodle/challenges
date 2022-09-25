bf937f8a31a6cc64c6ca758bdadec8f2087aa2dc
Madeleine <spyhoodle@icloud.com>
2022-09-25 10:20:24 +0100

    changing structure of book 3A challenge 8

--- Book-3A-Challenge-8/main.py
+++ Book-3A-Challenge-8/main.py
@@ -3,40 +3,46 @@ from tkinter import ttk
 import json
 
 
-class ConverterGUI(tk.Tk):
+class ConverterApp(tk.Tk):
     def __init__(self):
+        # Inherit from tk.Tk
         super().__init__()
 
         # Set up the window
-        self.frame = ttk.Frame(self, padding=10)
+        self.frame = ttk.Frame(self)
         self.title("Unit Converter")
         self.resizable(False, False)
-        self.geometry("300x200")
-        self.frame.grid(sticky="nsew")
+        self.frame.grid()
 
         # Parse units
         self.units = load_units("units.json")
 
+    def draw(self):
+        # Set grid orientation
+        self.value_input.grid(column=0, row=1, padx=10, pady=10)
+        self.value_output.grid(column=0, row=2, padx=10, pady=10)
+        self.label_input.grid(column=0, row=0, padx=10, pady=10)
+        self.unit_input.grid(column=1, row=1, padx=10, pady=10)
+        self.unit_output.grid(column=1, row=2, padx=10, pady=10)
+        self.button.grid(column=0, row=3, padx=10, pady=10)
+
+    def click(self):
+        print(self.value_input.get())
+
+
+class Widgets:
+    def __init__(self):
         # Input and output
         self.value_input = ttk.Entry(self, width=20)
         self.value_output = ttk.Entry(self, width=20, state="readonly")
 
         # Unit selection
+        self.label_input = ttk.Label(self, text="")
         self.unit_input = ttk.Combobox(self, values=self.units, width=5)
         self.unit_output = ttk.Combobox(self, values=self.units, width=5)
 
         # Button to convert
         self.button = ttk.Button(self, text="Convert", command=self.click)
-        self.button.grid(column=0, row=2)
-
-        # Set grid orientation
-        self.value_input.grid(column=0, row=0)
-        self.value_output.grid(column=0, row=1)
-        self.unit_input.grid(column=1, row=0)
-        self.unit_output.grid(column=1, row=1)
-
-    def click(self):
-        print(self.value_input.get())
 
 
 def load_json(file_name):
@@ -54,5 +60,5 @@ def load_units(file_name):
 
 
 if __name__ == "__main__":
-    app = ConverterGUI()
+    app = ConverterApp()
     app.mainloop()
