09897720b63b50f2611b3d79a36442ab9d92eefc
Spy <32415621+SpyHoodle@users.noreply.github.com>
2022-09-28 14:50:14 +0100

    challenge 11

---
+++ Book-3A-Challenge-11.py
@@ -0,0 +1,32 @@
+def sum_of_squares_of_digits(num):
+    return sum(int(digit) ** 2 for digit in str(num))
+
+
+def is_happy_number(num):
+    history = set()
+    while num != 1:
+        num = sum_of_squares_of_digits(num)
+        if num in history:
+            return False
+        else:
+            history.add(num)
+
+    return True
+
+
+def main():
+    num = 0
+    happy_nums = []
+
+    while len(happy_nums) < 8:
+        is_happy = is_happy_number(num)
+
+        if is_happy:
+            happy_nums.append(num)
+
+        num += 1
+    print(f"First {num} happy numbers:", happy_nums)
+
+
+if __name__ == "__main__":
+    main()
