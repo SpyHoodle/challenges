6ee71528c0f09a16e4af35144e35566a6a98adc4
Madeleine <spyhoodle@icloud.com>
2022-11-15 06:44:48 +0000

    working on date validator

--- Mini Projects/DateValidator.rs
+++ Mini Projects/DateValidator.rs
@@ -1,4 +1,73 @@
 use std::io::{self, Write};
+use std::collections::HashMap;
+
+enum Months {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December,
+}
+
+enum Date {
+    Year,
+    Month,
+    Day
+}
+
+fn incorrect_format() {
+    println!("Error: Please enter in the format YYYY-MM-DD")
+}
+
+fn is_leap(year: usize) -> bool {
+    if year % 400 == 0 {
+        true
+    } else if year % 4 == 0 && year % 100 != 0 {
+        true
+    } else {
+        false
+    };
+}
+
+fn valid(date: Vec<usize>) -> bool {
+    if date.len() != 3 {
+        incorrect_format()
+        return false;
+    } else if date[Date::Year].len() != 4 || date[Date::Month].len() != 2 || data[Date::Day].len() != 2 {
+        incorrect_format()
+        return false;
+    } else {
+        let days_in_months = HashMap::from([
+            (Months::January,   31),
+            (Months::February,  28),
+            (Months::March,     31),
+            (Months::April,     30),
+            (Months::May,       31),
+            (Months::June,      30),
+            (Months::July,      31),
+            (Months::August,    31),
+            (Months::September, 30),
+            (Months::October,   31),
+            (Months::November,  30),
+            (Months::December,  31),
+        ]);
+
+        if is_leap(date[Date::Year]) {
+            let feb = days_in_months.entry(Months::Februrary).and_modify(|feb| *feb = 29).or_insert(29);
+        };
+
+        if date[Date::Month].parse::<usize>().unwrap() > 12
+
+        return true;
+    }
+}
 
 fn main() {
     print!("Enter a date in the format YYYY-MM-DD: ");
@@ -8,23 +77,12 @@ fn main() {
         .read_line(&mut buffer)
         .unwrap();
 
-    let date: Vec<u8> = buffer.trim_end().split("-").as_int().collect();
+    let date: Vec<&str> = buffer.trim_end().split("-").collect();
+    let result = valid(date);
 
-    if date.len() != 3 {
-        println!("Error: Please enter in the format YYYY-MM-DD")
-    }
-    else if date[0].as_str().len() != 4 {
-        println!("Error: Please enter in the format YYYY-MM-DD")
-    }
-    else if date[1].as_str().len() != 2 {
-        println!("Error: Please enter in the format YYYY-MM-DD")
-    }
-    else if date[2].as_str().len() != 2 {
-        println!("Error: Please enter in the format YYYY-MM-DD")
-    }
-
-    for item in date {
-        println!("{}", item)
-    }
-
+    if result {
+        println!("Valid!");
+    } else {
+        println!("Invalid!");
+    };
 }
